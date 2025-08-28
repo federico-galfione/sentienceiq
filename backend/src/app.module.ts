dc86bc43f83286ab694089e3fcf66f205e1f3dc0
Federico Galfione <federico.galfione@outlook.com>
2025-08-28 11:49:16 +0200

    feat: remove mongo and add postgre

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url:
+        process.env.DATABASE_URL ||
+        'postgresql://appuser:apppass@localhost:5432/mydb',
+      autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV !== 'production',
+    }),
+  ],
   controllers: [],
   providers: [],
 })
